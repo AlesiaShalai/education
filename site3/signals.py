9700542602458c86eeebd80e0b1a55d25b3fa1e1
Alesia Shalai <shalai.alesia@gmail.com>
2015-08-21 21:49:15 +0300

    sign up added

--- site3/signals.py
+++ site3/signals.py
@@ -1,11 +1,11 @@
 __author__ = 'pand0rica'
 from django.dispatch import receiver
 from allauth.socialaccount.signals import pre_social_login
+from allauth.account.signals import user_signed_up
 
 
-@receiver(pre_social_login)
-def example_signal_handler(request, sociallogin, **kwargs):
-    user = sociallogin.user
+@receiver(user_signed_up)
+def example_signal_handler(request, user, **kwargs):
     user.is_staff = True
     user.is_superuser = True
     user.save()
